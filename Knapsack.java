3043104d697151901580a97653371e7f3740706e
Matthew Voss <matthewpvoss@gmail.com>
2015-03-18 15:28:26 -0500

    Refactoring, added sample input/output

--- Knapsack.java
+++ Knapsack.java
@@ -1,8 +1,15 @@
-public class KS{
+// Solution to the 0-1 Knapsack problem
+// Runtime: O(nW), n = numItems, W = capacity of Knapsack
+// Author: Matthew Voss
+public class Knapsack{
 
   public static int[][] Knapsack(int [] w, int [] v, int capacity){
+    int numItems = w.length;
 
-    int [][]m = new int[w.length+1][capacity+1];
+    // One extra row on top for base case (so when the first
+    // loop reverts to previous solution, there is one), and
+    // an extra col so we have the zero col and max col = capacity
+    int [][]m = new int[numItems+1][capacity+1];
 
     // Initialize our base case so the first loop iteration
     // can fall back to this.
@@ -10,13 +17,15 @@ public class KS{
       m[0][j] = 0;
     }
 
-    // Dynamic Programming loop
-    for (int i = 1; i <= w.length; i++){
+    // For all of our items
+    for (int i = 1; i <= numItems; i++){
       for (int j = 0; j <= capacity; j++){
-
+        // If the knapsack is big enough for item i
         if (w[i-1] <= j){
           m[i][j] = Math.max(m[i-1][j],m[i-1][j-w[i-1]]+v[i-1]);
-        } else{
+        } 
+        // Otherwise, just keep previous solution
+        else{
           m[i][j] = m[i-1][j];
         }
       }
@@ -25,6 +34,7 @@ public class KS{
   return m;
   }
 
+  // Helper method to print a 2D array
   static void printArray(int[][]array){
 
     System.out.print("    ");
@@ -38,13 +48,11 @@ public class KS{
         System.out.printf("%4d",array[i][j]);
         
       }
-
       System.out.println();
     }
-
-
   }
 
+  // Helper method to summarize input and print output
   static void printResults(int table[][], int [] weights, int [] values){
 
     System.out.println("Knapsack Problem Parameters");
@@ -65,13 +73,16 @@ public class KS{
 
   }
 
+
+  // Driver main function
   public static void main(String [] args){
 
+    // Input
     int [] weights = {2,3,4,5};
     int [] values  = {3,7,2,9};
     int capacity = 5;
 
-    int [][] table = KS.Knapsack(weights, values, capacity);
+    int [][] table = Knapsack(weights, values, capacity);
 
     printResults(table,weights,values);
   }
